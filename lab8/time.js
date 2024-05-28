d7f7ebb75748312e267420ec2aac112667e6b95a
Denis Chervonyak <mestoush@mail.ru>
2024-05-28 21:01:14 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -4,5 +4,8 @@ function showTime() {
     let currentSec = today.getSeconds()
     document.getElementById('time').innerHTML = currentTime;
     document.getElementById('bigTime').innerHTML = currentSec;
+    let degrees = (currentSec / 60) * 360;
+    let secondHand = document.getElementById('secondHand');
+    secondHand.style.transform = `rotate(${degrees}deg)`;
 }
 setInterval(showTime, 1000)
