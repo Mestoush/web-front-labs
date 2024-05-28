fe7e64d075ffd6351c5611c33f3a4beb6580c228
Denis Chervonyak <mestoush@mail.ru>
2024-05-28 19:35:02 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -15,4 +15,17 @@ function showDate() {
         let dateString = today.toLocaleString(locale);
         out.innerHTML += `<p>Дата и время для страны ${name}: ${dateString}</p>`;
     });
+
+    let year = today.getFullYear();
+    let month = today.toLocaleString('default', { month: 'long' }); 
+    let date = today.getDate();
+    let day = today.toLocaleString('default', { weekday: 'long' }); 
+
+    out.innerHTML += `
+        <p>Текущий год: ${year}</p>
+        <p>Текущий месяц: ${month}</p>
+        <p>Текущая дата: ${date}</p>
+        <p>День недели: ${day}</p>
+        `
 }
+
