1dd7e73f197080a511cb402b07b64438dac1a520
Denis Chervonyak <mestoush@mail.ru>
2024-05-28 19:25:10 +0700

    Вывод даты

---
+++ lab8/lab8.js
@@ -0,0 +1,18 @@
+function showDate() {
+    let out = document.getElementById('current-date')
+    let today = new Date();
+    
+    let locales = [
+        {locale: 'ru-RU', name: 'Россия'},
+        {locale: 'en-US', name: 'США'},
+        {locale: 'fr-FR', name: 'Франция'},
+        {locale: 'de-DE', name: 'Германия'},
+        {locale: 'zh-CN', name: 'Китай'},
+        {locale: 'ja-JP', name: 'Япония'}
+    ];
+
+    locales.forEach(({locale, name}) => {
+        let dateString = today.toLocaleString(locale);
+        out.innerHTML += `<p>Дата и время для страны ${name} (${locale}): ${dateString}</p>`;
+    });
+}
